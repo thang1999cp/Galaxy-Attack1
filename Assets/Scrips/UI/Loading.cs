a9abd25fa5557d95380f891ded69ff2418464125
thang1999cp <151833741+thang1999cp@users.noreply.github.com>
2025-05-28 15:37:52 +0700

    log

---
+++ Assets/Scrips/UI/Loading.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    public Slider progressBar;
+    public float loadingDuration = 3f;
+
+    private float timer = 0f;
+
+    void Start()
+    {
+        progressBar.value = 0f;
+        StartCoroutine(nameof(LoadingBar));
+    }
+
+    private IEnumerator LoadingBar()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < loadingDuration)
+        {
+            elapsed += Time.deltaTime;
+            float progress = Mathf.Clamp01(elapsed / loadingDuration);
+            progressBar.value = progress;
+            yield return null;
+        }
+
+        progressBar.value = 1f;
+    }
+}
